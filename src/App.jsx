4d179300a46838e85cd29eee83d6ec1276acea64
unknown <mahdiikeshvarii@gmail.com>
2024-12-05 17:33:15 +0330

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import ProductCard from './productCard/product.jsx';
+
+function App() {
+  return (  
+    <div>
+     <ProductCard />
+   </div>
+  )
+}
+
+export default App
